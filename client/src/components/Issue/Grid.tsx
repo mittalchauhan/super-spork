a6810908210503962a6b8d8ee52ced7be85801ff
dhiraj <dhiraj.chhetri2007@gmail.com>
2023-04-21 00:08:31 +0530

    grid

--- client/src/components/Issue/Grid.tsx
+++ client/src/components/Issue/Grid.tsx
@@ -1,65 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { getIssues } from '../../utils/API/issue_API';
-// import { DateFormat } from 'primereact/dateformat';
+import React, { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
+export default function Grid(props) {
+  const { columns, data, total, isLoading } = props;
 
-export default function Grid() {
-    const [loading, setLoading] = useState(false);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const [issues, setissues] = useState(null);
-    const [selectAll, setSelectAll] = useState(false);
-    const [selectedissues, setSelectedissues] = useState(null);
-    const [lazyState, setlazyState] = useState({
-        first: 0,
-        rows: 10,
-        page: 1,
-        sortField: null,
-        sortOrder: null,
-      
-    });
+  const dateTemplate = (rowData, column) => {
+    return new Date(rowData["createdAt"]).toLocaleDateString();
+  };
 
-
-    useEffect(() => {
-        loadLazyData();
-    }, [lazyState]);
-
-    const loadLazyData = () => {
-        setLoading(true);
-            getIssues().then((data) => {
-                setTotalRecords(data.totalRecords);
-                setissues(data.data);
-                setLoading(false);
-            });
-      
-    };
-    const dateTemplate=(rowData, column) =>{
-     return new Date (rowData['createdAt']).toLocaleDateString();
+  return (
+    <DataTable
+      value={data}
+      tableStyle={{ minWidth: "50rem" }}
+      rows={10}
+      totalRecords={total}
+      paginator
+      loading={isLoading}
+    >
+      {columns.map(({ field, header, type }) => {
+        return type === "date" ? (
+          <Column field={field} body={dateTemplate} header={header} />
+        ) : (
+          <Column field={field} header={header} />
+        );
+      })}
+    </DataTable>
+  );
 }
-
-    const onPage = (event) => {
-        setlazyState(event);
-    };
-
-
-
-    return (
-        <div className="card">
-            <DataTable value={issues} lazy filterDisplay="row" dataKey="id" paginator
-                    first={lazyState.first} rows={10} totalRecords={totalRecords} onPage={onPage}
-                     sortField={lazyState.sortField} sortOrder={lazyState.sortOrder}
-                     loading={loading} tableStyle={{ minWidth: '75rem' }}
-                    selection={selectedissues}  selectAll={selectAll} >
-                <Column field="summary" header="Summary"   filterPlaceholder="Search" />
-                <Column field="description"  header="Description"  />
-                <Column field="projectId.name"   header="Project" />
-                <Column field="creatorId.firstname" header="Created By"   />
-                <Column field="issueTypeId.label" header="Issue type"   />
-                <Column field="priorityId.label" header="Priority"   />
-                <Column field="createdat" body={dateTemplate} header="created on"  />
-            </DataTable>
-        </div>
-    );
-}
-         
--- client/src/pages/Issue.tsx
+++ client/src/pages/Issue.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'primereact/button';
 
 import Grid from '../components/Issue/Grid'
@@ -8,25 +8,61 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 //core
 import "primereact/resources/primereact.min.css";
+import CreateIssueModal from "../components/Issue/CreateModal";
+import { getIssues } from '../utils/API/issue_API';
 
-function Issue() {
+
+function Issue(props) {
+  const { currentUser, logoutUser } = props;
+    const [isIssueOpen, setIssueOpen] = useState(false);
+  const [totalRecords, setTotalRecords]= useState(0)
+  const [issues, setIssues]= useState([])
+  const [loading, setLoading]= useState(false)
+const issueColumns=[
+{field:"description", header:"Description", type:"string"},
+{field:"projectId.name", header:"Project", type:"string"},
+{field:"creatorId.firstname", header:"Created By", type:"string"},
+{field:"issueTypeId.label", header:"Issue type", type:"string"},
+{field:"priorityId.label", header:"Priority", type:"string"},
+{field:"createdat", header:"Created on", type:"date"},
+
+]
+
   useEffect(()=>{
-    
+     getIssues().then((data) => {
+                setTotalRecords(data.totalRecords);
+                setIssues(data.data);
+                setLoading(false);
+            });
   },[])
+  
   return (
     <Main>
+        
+      {isIssueOpen && (
+        <CreateIssueModal
+          isOpen={isIssueOpen}
+          handleClose={() => setIssueOpen(false)}
+          currentUser={currentUser}
+        />
+      )}
         <div className='grid'>
-          <div className='col-lg-4'>
+          <div className='col-md-4'>
           <h1>Issues</h1>
 
           </div>
-          <div className='lg-offset-6 col-lg-2'>
-          <Button label="create new issue" />
+          <div className='md-offset-6 col-md-2'>
+          <Button label="create new issue" onClick={()=>setIssueOpen(true)}/>
 
           </div>
 
           </div>
-        <Grid />
+        <Grid 
+        totalRecords={totalRecords}
+        data={issues}
+        isLoading={loading}
+        columns={issueColumns}
+        />
 
     </Main>
   )
